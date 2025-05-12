2dab44ee39db1212e9202b5c032efc8f5df683b4
vishal911m <vishal911m@gmail.com>
2025-05-12 16:44:07 +0530

    feat: server deployed

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -2,68 +2,50 @@ import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import User from "../models/auth/UserModel.js";
 
+// Protect middleware
 export const protect = asyncHandler(async (req, res, next) => {
   try {
-    // check if user is logged in
     const token = req.cookies.token;
 
     if (!token) {
-      // 401 Unauthorized
-      res.status(401).json({ message: "Not authorized, please login!" });
+      return res.status(401).json({ message: "Not authorized, please login!" });
     }
 
-    // verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // get user details from the token ----> exclude password
     const user = await User.findById(decoded.id).select("-password");
 
-    // check if user exists
     if (!user) {
-      res.status(404).json({ message: "User not found!" });
+      return res.status(404).json({ message: "User not found!" });
     }
 
-    // set user details in the request object
     req.user = user;
-
     next();
   } catch (error) {
-    // 401 Unauthorized
-    res.status(401).json({ message: "Not authorized, token failed!" });
+    return res.status(401).json({ message: "Not authorized, token failed!" });
   }
 });
 
-// admin middleware
+// Admin middleware
 export const adminMiddleware = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.role === "admin") {
-    // if user is admin, move to the next middleware/controller
-    next();
-    return;
+    return next();
   }
-  // if not admin, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only admins can do this!" });
+  return res.status(403).json({ message: "Only admins can do this!" });
 });
 
+// Creator middleware
 export const creatorMiddleware = asyncHandler(async (req, res, next) => {
-  if (
-    (req.user && req.user.role === "creator") ||
-    (req.user && req.user.role === "admin")
-  ) {
-    // if user is creator, move to the next middleware/controller
-    next();
-    return;
+  if (req.user && (req.user.role === "creator" || req.user.role === "admin")) {
+    return next();
   }
-  // if not creator, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only creators can do this!" });
+  return res.status(403).json({ message: "Only creators can do this!" });
 });
 
-// verified middleware
+// Verified middleware
 export const verifiedMiddleware = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.isVerified) {
-    // if user is verified, move to the next middleware/controller
-    next();
-    return;
+    return next();
   }
-  // if not verified, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Please verify your email address!" });
+  return res.status(403).json({ message: "Please verify your email address!" });
 });
