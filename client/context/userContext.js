f9d4a034ab8d94090b5ce913b327052dbf895c29
vishal911m <vishal911m@gmail.com>
2025-05-12 13:42:13 +0530

    server deployed

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,8 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  // const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://monitr-fm9f.onrender.com";
 
   const router = useRouter();
 
