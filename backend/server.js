c343da9549b56701b84b6ff63a1bd675b4b3df26
vishal911m <vishal911m@gmail.com>
2025-05-13 15:36:54 +0530

    server deployed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: ["http://localhost:3000", "https://monitr-livid.vercel.app"],
     credentials: true,
   })
 );
