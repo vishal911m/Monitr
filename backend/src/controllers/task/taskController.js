4343a1b0bd7e5805aaf23511671064c3968081cd
vishal911m <vishal911m@gmail.com>
2025-07-14 14:00:28 +0530

    server deployed

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -20,8 +20,14 @@ export const createTask = asyncHandler(async (req,res)=>{
 
     res.status(201).json(task);
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: `Task with the title "${req.body.title}" already exists for this user.`,
+      });
+    }
+
     console.log("Error in createTask: ", error.message);
-    res.status(500).json({message: error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
